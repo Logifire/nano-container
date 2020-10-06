010c5862fbe68985cad38c459a6c5be7217d3019
Logifire <Logifire@users.noreply.github.com>
2020-10-06 21:45:11 +0200

    Added doc-block description to register and set method

--- src/ContainerFactory.php
+++ src/ContainerFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace NanoContainer;
 
 use Closure;
@@ -11,11 +12,23 @@ class ContainerFactory extends ContainerDecorator implements Factory
      */
     protected $factory_stack = [];
 
+    /**
+     * @param string  $id      Example::class
+     * @param Closure $closure function(Container $c) {...return $example;}
+     * 
+     * @return void
+     */
     public function register(string $id, Closure $closure): void
     {
         $this->factory_stack[$id] = $closure;
     }
 
+    /**
+     * @param string $id    Example::class
+     * @param type   $value $example
+     * 
+     * @return void
+     */
     public function set(string $id, $value): void
     {
         $this->factory_stack[$id] = $value;
